0a20739dba5031eb1bba73a30607c820ed744ec7
mert99 <472114271@qq.com>
2012-11-26 01:21:29 +0800

    add atom/rss feeds

--- settings.py
+++ settings.py
@@ -15,7 +15,10 @@ AUTHOR = 'mert99'
 DISQUS_SITENAME = 'mert99sblog'
 GITHUB_URL = 'https://github.com/mert99'
 SITEURL = 'http://mert99.github.com'
+FEED_ATOM = 'feeds/all.atom.xml'
+FEED_RSS = 'feeds/all.rss.xml'
 TAG_FEED_ATOM = 'feeds/%s.atom.xml'
+TAG_FEED_RSS = 'feeds/%s.rss.xml'
 
 SOCIAL = (('github', 'https://github.com/mert99'), 
          )
